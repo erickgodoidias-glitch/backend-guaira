0ecaf2d5074f9d02003285cabf4b788527ff566e
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-03-10 14:37:17 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço completo");
+
+let rua = "Rua das flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco);
+console.log(" O endereço correto é: " + endereco);
