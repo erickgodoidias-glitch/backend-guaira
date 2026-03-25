b48e540bff16f6fb3e3e74d6c11dc80733996139
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-03-25 18:38:41 +0000

    igygyg

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,5 @@ let tempCelsius = 18.5;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempKelvin = TempCelsius + 273.15;
+
+console.log(`A temperatura de ${cidade} está ${tempCelsius} Cº`);
