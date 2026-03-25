e03b0a449074dbdf7b722f3469ce419ac0c975ed
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-03-25 18:48:19 +0000

    versao2

--- temperatura.js
+++ temperatura.js
@@ -1,8 +1,10 @@
 //conversor de temperaturas
 const cidade = "Curitiba"
-let tempCelsius = 18.5;
+let tempCelsius = 26;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempKelvin = TempCelsius + 273.15;
 
 console.log(`A temperatura de ${cidade} está ${tempCelsius} Cº`);
+console.log(`Fahreheit: ${tempFahrenheit.toFixed(1)} Fº`)
+console.log(`kelvin: ${tempFahrenheit.toFixed(1)} kº`)
