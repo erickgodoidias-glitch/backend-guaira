493165a1985f01e3df73d351ad883138d419d2ca
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-03-31 17:28:06 +0000

    hvhvhvh

--- operadores.js
+++ operadores.js
@@ -1,10 +1,19 @@
 //OPERADOR E &&
 let maiorIdade = true; 
-let possuiDocumento = false;
+let possuiDocumento = true;
 
 if (maiorIdade && possuiDocumento) {
     console.log('Pode entrar na festa!')
-
 } else {
     console.log('Não pode entrar na festa.')
 };
+
+//OPERADOR OU ||
+let cartaoCredito = false;
+let dinheiro = true 
+
+if (cartaoCredito || dinheiro) {
+    console.log('Pode fazer a compra')
+} else {
+    console.log('')
+}
