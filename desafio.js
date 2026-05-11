bbaf74ec61b66723c5e40de9ba6b5d843d05f41f
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-05-11 15:42:48 -0300

    kçh3rklhflk

---
+++ desafio.js
@@ -0,0 +1,11 @@
+let idade = 15;
+let nome = "Erick";
+let serie = "1 A";
+let nascimento = 2010;
+let cpf = "122.155.134-14";
+
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Série:", serie);
+console.log("Ano de nascimento:", nascimento);
+console.log("CPF:", cpf);
