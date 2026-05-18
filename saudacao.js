c73e075c019a685898eff2f1fb65e2727b7b84c0
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-05-18 18:15:47 +0000

    iuytredcvbnm,.

--- saudacao.js
+++ saudacao.js
@@ -1,8 +1,18 @@
+<<<<<<< HEAD
+let filaEspera = ["ana", "Bruno", "Carla"];
+=======
 let filaEspera = ["ana", "Bruno", "Carla"];
 console.log(filaEspera);
+>>>>>>> dbae55bb76c7a4bc7579bbfdb7cbc4dbc953d9e5
 
+<<<<<<< HEAD
+console.log(filaEspera);
+filaEspera.push("Diego");
+console.log(filaEspera);
+=======
 filaEspera.push("Diego");
 console.log(filaEspera);
 
 filaEspera.shift();
 console.log(filaEspera);
+>>>>>>> dbae55bb76c7a4bc7579bbfdb7cbc4dbc953d9e5
