4fe8857121f3648c052d737ee6c73198c2b3d5a4
erickgodoidias-glitch <erick.godoi.dias@escola.pr.gov.br>
2026-05-18 18:46:33 +0000

    çljukgjfjghfgthf

--- desafio.js
+++ desafio.js
@@ -1,10 +1,10 @@
 let idade = 16;
 let nome = "Erick";
-let serie = "2 A";
+let serie = "2 ";
 let nascimento = 2010;
 let cpf = "122.155.134-14";
-let soma = "idade"; 
-let multiplicação = "nascimento"
+let soma = "nota"; 
+let nota = "6,7", 
 
 console.log("Nome:", nome);
 console.log("Idade:", idade);
@@ -12,4 +12,5 @@ console.log("Série:", serie);
 console.log("Ano de nascimento:", nascimento);
 console.log("CPF:", cpf);
 console.log("soma", soma)
+comsole.log()
 console.log(`Nome: ${nome}, Idade: ${idade}, Série: ${serie}, Ano de nascimento: ${nascimento}, CPF: ${cpf}. você concluiu o cadastro. ${soma} + ${serie} ° ${nascimento} = 36,180 `);
